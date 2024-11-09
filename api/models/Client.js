ea45463545205f0d0005667d03f789d351bf72a3
HudsonCarneiro <161895030+HudsonCarneiro@users.noreply.github.com>
2024-11-09 10:47:31 -0300

    refactor Client.js

--- api/models/Client.js
+++ api/models/Client.js
@@ -2,37 +2,46 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const Client = sequelize.define('Client', {
-  businessName: {
-      type: DataTypes.STRING,
-      allowNull: false
-  },
-  companyName: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  cnpj:{
-    type: DataTypes.STRING
-  },
-  phone:{
-    type: DataTypes.STRING
-  },
-  email: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false
-  },
+    businessName: {
+        type: DataTypes.STRING(255), // Limite de 255 caracteres
+        allowNull: false
+    },
+    companyName: {
+        type: DataTypes.STRING(255), // Limite de 255 caracteres
+        allowNull: false
+    },
+    cnpj: {
+        type: DataTypes.STRING(14), // CNPJ tem 14 dígitos
+        allowNull: false,
+        validate: {
+            isNumeric: true, // Garante que só aceita números
+            len: [14, 14] // Exige exatamente 14 caracteres
+        }
+    },
+    phone: {
+        type: DataTypes.STRING(15), // Limite para o telefone
+        allowNull: true,
+        validate: {
+            is: /^\(?\d{2}\)?[\s-]?[\s9]?\d{4}-?\d{4}$/ // Validação para telefone brasileiro
+        }
+    },
+    email: {
+        type: DataTypes.STRING(100), // Limite de 100 caracteres para o email
+        unique: true,
+        allowNull: false,
+        validate: {
+            isEmail: true // Garante que o valor é um email válido
+        }
+    }
 });
 
 Client.associate = (models) => {
-  Client.hasMany(models.Freight, {
-    foreignKey: 'clientId'
-  })
-}
-
-Client.associate = (models)=> {
-  Client.belongsTo(models.Address,{
-      foreignKey: 'addressId'
-  });
-}
+    Client.hasMany(models.Freight, {
+        foreignKey: 'clientId'
+    });
+    Client.belongsTo(models.Address, {
+        foreignKey: 'addressId'
+    });
+};
 
 module.exports = Client;
