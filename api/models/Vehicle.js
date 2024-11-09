374316085ae46dd3a946422d621dce5e2755f575
HudsonCarneiro <161895030+HudsonCarneiro@users.noreply.github.com>
2024-11-09 17:23:38 -0300

    refactor Vehicle.js

--- api/models/Vehicle.js
+++ api/models/Vehicle.js
@@ -2,28 +2,42 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const Vehicle = sequelize.define('Vehicle', {
-  model: {
-      type: DataTypes.STRING,
-      allowNull: false
-  },
-  automaker:{
-    type: DataTypes.STRING
-  },
-  year:{
-    type: DataTypes.INTEGER
-  },
-  fuelType:{
-    type: DataTypes.STRING
-  },
-  mileage:{
-    type: DataTypes.INTEGER
-  },
+    model: {
+        type: DataTypes.STRING(100), // Limite de 100 caracteres
+        allowNull: false
+    },
+    automaker: {
+        type: DataTypes.STRING(100), // Limite de 100 caracteres
+        allowNull: true
+    },
+    year: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        validate: {
+            min: 1900, // Ano mínimo
+            max: new Date().getFullYear() // Não pode ser maior que o ano atual
+        }
+    },
+    fuelType: {
+        type: DataTypes.STRING(50), // Limite de 50 caracteres
+        allowNull: true,
+        validate: {
+            isIn: [['Gasolina', 'Diesel', 'Etanol', 'Flex', 'Elétrico']] // Valores permitidos
+        }
+    },
+    mileage: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        validate: {
+            min: 0 // A quilometragem não pode ser negativa
+        }
+    },
 });
 
 Vehicle.associate = (models) => {
-  Vehicle.hasMany(models.Fueling, {
-    foreignKey: 'vehicleId'
-  })
-}
+    Vehicle.hasMany(models.Fueling, {
+        foreignKey: 'vehicleId'
+    });
+};
 
 module.exports = Vehicle;
