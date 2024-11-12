d5578187dd8802a3845fc89ef58582bdb902403d
HudsonCarneiro <161895030+HudsonCarneiro@users.noreply.github.com>
2024-11-12 13:50:08 -0300

    Delivery.js create

--- api/models/Delivery.js
+++ api/models/Delivery.js
@@ -1,18 +1,18 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
-const Freight = sequelize.define('Freight', {
+const Delivery = sequelize.define('Delivery', {
     status: {
         type: DataTypes.STRING, // aguardando / em transito / finalizado
         allowNull: false,
     },
 });
 
-// Associações do modelo Freight
-Freight.associate = (models) => {
-    Freight.belongsTo(models.Client, { foreignKey: 'clientId' });
+// Associações do modelo Delivery
+Delivery.associate = (models) => {
+    Delivery.belongsTo(models.Client, { foreignKey: 'clientId' });
     Freight.belongsTo(models.Driver, { foreignKey: 'driverId' });
     Freight.belongsTo(models.Vehicle, { foreignKey: 'vehicleId' });
 };
 
-module.exports = Freight;
+module.exports = Delivery;
