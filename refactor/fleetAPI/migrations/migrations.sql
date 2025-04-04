92af3541bdb5d0820af9e05e947eb35356df31d5
Hudson <161895030+HudsonCarneiro@users.noreply.github.com>
2025-04-04 19:45:37 -0300

    delete refactor

--- refactor/fleetAPI/migrations/migrations.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE drivers (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    cnh VARCHAR(25) NOT NULL UNIQUE,
-    phone VARCHAR(25) NULL,
-    company_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-);
--- refactor/fleetAPI/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getDashboard } = require('../controllers/dashboardController');
-const { authenticateToken } = require('../middlewares/authMiddleware');
-
-// Rota protegida
-router.get('/dashboard', authenticateToken, getDashboard);
-
-module.exports = router;
