93270185e526d84644e4d473f98613b7e8ab6978
HudsonCarneiro <161895030+HudsonCarneiro@users.noreply.github.com>
2025-04-05 17:26:37 -0300

    refactor legado

--- legado/AppWEB/API/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getDashboard } = require('../controllers/dashboardController');
-const { authenticateToken } = require('../middlewares/authMiddleware');
-
-// Rota protegida: o middleware é executado antes do controlador
-router.get('/dashboard', authenticateToken, getDashboard);
-
-module.exports = router;
--- legado/frontEnd/src/js/model/User.js
+++
@@ -1,9 +0,0 @@
-export default class User{
-    constructor(name, cpf, phone, email, password){
-        this.name = name;
-        this.cpf = cpf;
-        this.phone = phone;
-        this.email = email;
-        this.password = password;
-    }
-}
