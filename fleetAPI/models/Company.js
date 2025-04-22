59709e2797deb38bc17443ff230a748fef491ffb
HudsonCarneiro <161895030+HudsonCarneiro@users.noreply.github.com>
2025-04-22 13:45:29 -0300

    company controller init

--- fleetAPI/models/Company.js
+++ fleetAPI/models/Company.js
@@ -7,5 +7,25 @@ const Company = require.define('Company',{
         autoincrement: true,
         primaryKey: true,
     },
+    cnpj: {
+        type: DataTypes.STRING(14),
+        allowNull: true,
+    },
+    companyName: {
+        type: DataTypes.STRING(55),
+        allowNull: false,
+    },
+    businessName: {
+        type: DataTypes.STRING(55),
+        allowNull: false,
+    },
+    addressId: {
+        type: DataTypes.INTEGER,
+        allowNull: false, 
+        references: {
+          model: 'Addresses', // Nome da tabela Address no banco
+          key: 'id',
+        },
+    }
 
 })
